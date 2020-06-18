dceeed5955371db04773c850c30f90b21198eeb1
garodon21aque <64726247+garodon21aque@users.noreply.github.com>
2020-06-18 15:10:39 +0100

    Create Gui_main.py

---
+++ Gui_main.py
@@ -0,0 +1,33 @@
+#test application for a potential Gym app for private use.
+from tkiner import *
+from PIL import ImageTk, Image	
+		
+window = Tk()
+window.title("Tablet App1.0")
+window.configure(background="black")
+def click()
+    
+    
+    
+Label (window, text="My first app", bg="black", fg="white", font="none 12 bold") .grid(row=0, column=0, sticky=W)
+
+Button (window, text="Test", width=6, command=click) .grid(row=1, column=0, sticky=W)
+#Test button will be testing the first kind of excercise and the following number will be the other excercises.
+  
+Button (window, text="Test2", width=6, command=click) .grid(row=1, column=1, sticky=M)
+
+Button (window, text="Test3", width=6, command=click) .grid(row=1, column=2, sticky=E)
+
+#Calendar time
+
+
+
+
+
+
+window.mainloop	
+
+
+
+ 
+
